2a21b21722512953bdf139b6488f510c8072f9e1
awank <yophy@kendari.linux.or.id>
2018-11-02 10:44:49 +0800

    upload

--- application/config/common/dp_config.php
+++ application/config/common/dp_config.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['host_dev'] = array('localhost', '127.0.0.1', '::1');
 
 // Fill in the file of your project here when you develop locally.
-$host_dev = 'https://app-natural.herokuapp.com/';
+$host_dev = 'app-natural';
 
 // Fill in the domain name here when your project is online.
 // Example : www.johndoe.com
