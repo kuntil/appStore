53f5b5e01832ae2d65174336e892b0e3e7076a32
awank <yophy@kendari.linux.or.id>
2018-11-27 11:20:01 +0800

    bug fix, add some function

--- application/models/admin/Supplier_model.php
+++ application/models/admin/Supplier_model.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Supplier_model extends CI_Model {
 
-    string $table = 'supplier_tbl';
-    string $column_order = array(null, 'supplier_id','supplier_name','address','telp'); //set column field database for datatable orderable
-    string $column_search = array('supplier_id','supplier_name','address','telps'); //set column field database for datatable searchable 
-    string $order = array('supplier_id' => 'asc'); // default order 
+    private String $table = 'supplier_tbl';
+    private String $column_order = array(null, 'supplier_id','supplier_name','address','telp'); //set column field database for datatable orderable
+    private String $column_search = array('supplier_id','supplier_name','address','telps'); //set column field database for datatable searchable 
+    private String $order = array('supplier_id' => 'asc'); // default order 
 
     public function __construct()
     {
