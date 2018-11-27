832b6f08d9ad53f976db23fe171fbf22b41ac101
awank <yophy@kendari.linux.or.id>
2018-11-27 12:51:51 +0800

    bug fix, add some function

--- application/views/admin/supplier/index.php
+++ application/views/admin/supplier/index.php
@@ -43,34 +43,37 @@
                 </section>
             </div>
 
+ 
 <script type="text/javascript">
 $.noConflict();
 var table;
+(function($){
+    $(document).ready(function() {
  
-$(document).ready(function() {
- 
-    //datatables
-    table = $('#table').DataTable({ 
- 
-        "processing": true, //Feature control the processing indicator.
-        "serverSide": true, //Feature control DataTables' server-side processing mode.
-        "order": [], //Initial no order.
- 
-        // Load data for the table's content from an Ajax source
-        "ajax": {
-            "url": "<?php echo site_url('admin/supplier/ajax_list')?>",
-            "type": "POST"
-        },
- 
-        //Set column definition initialisation properties.
-        "columnDefs": [
-        { 
-            "targets": [ 0 ], //first column / numbering column
-            "orderable": false, //set not orderable
-        },
-        ],
- 
-    });
- 
+ //datatables
+ table = $('#table').DataTable({ 
+
+     "processing": true, //Feature control the processing indicator.
+     "serverSide": true, //Feature control DataTables' server-side processing mode.
+     "order": [], //Initial no order.
+
+     // Load data for the table's content from an Ajax source
+     "ajax": {
+         "url": "<?php echo site_url('admin/supplier/ajax_list')?>",
+         "type": "POST"
+     },
+
+     //Set column definition initialisation properties.
+     "columnDefs": [
+     { 
+         "targets": [ 0 ], //first column / numbering column
+         "orderable": false, //set not orderable
+     },
+     ],
+
+ });
+
 });
+})
+
 </script>
