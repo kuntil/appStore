7401031aa0a157f835943191f462d5bcbf89c864
awank <yophy@kendari.linux.or.id>
2019-02-25 18:06:49 +0800

    upload modul gudang

--- application/config/common/dp_config.php
+++ application/config/common/dp_config.php
@@ -20,7 +20,7 @@ $host_dev = 'app-natural';
 
 // WARNING: Do not modify the lines below
 //$domain = (in_array($_SERVER['HTTP_HOST'], $config['host_dev'], TRUE)) ? $_SERVER['HTTP_HOST'] . '/' . $host_dev : $host_prod;
-
+/* pastikan kalo ini tetep terblok untuk serting server */
 //$config['base_url'] = ( ! empty($_SERVER['HTTPS'])) ? 'https://' . $domain : 'http://' . $domain;
 
 $config['base_url']="https://app-natural.herokuapp.com/";
